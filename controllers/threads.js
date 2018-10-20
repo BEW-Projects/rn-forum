00dce9f083ff60516e36b5a1e182209f3740d0ec
Edwin Cloud <ecloud412@gmail.com>
2018-10-20 16:48:21 -0700

    :zap: Threads and Users get method completed

--- controllers/threads.js
+++ controllers/threads.js
@@ -1,6 +1,29 @@
 const router = require('express').Router();
+const { authorizedHandler, validQueryHandler } = require('../middlewares');
 const { thread } = require('../models');
 
 // routes below
+router.route('/threads')
+  .all(authorizedHandler, validQueryHandler(Object.keys(thread.schema.paths)))
+  .get((req, res, next) => {
+    if(Object.keys(req.query).length == 0) {
+      thread.find().lean().then(threads => {
+        res.json(threads);
+      })
+    } else {
+      thread.find(req.query).lean().then(threads => {
+        res.json(threads);
+      })
+    }
+  })
+  .post((req, res) => {
+
+  })
+  .put((req, res) => {
+
+  })
+  .delete((req, res) => {
+
+  });
 
 module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,29 @@
 const router = require('express').Router();
+const { authorizedHandler, validQueryHandler } = require('../middlewares');
 const { user } = require('../models');
 
 // routes below
+router.route('/users')
+  .all(authorizedHandler, validQueryHandler(Object.keys(category.schema.paths)))
+  .get((req, res, next) => {
+    if(Object.keys(req.query).length == 0) {
+      user.find().lean().then(users => {
+        res.json(users);
+      })
+    } else {
+      user.find(req.query).lean().then(users => {
+        res.json(users);
+      })
+    }
+  })
+  .post((req, res) => {
+
+  })
+  .put((req, res) => {
+
+  })
+  .delete((req, res) => {
+
+  });
 
 module.exports = router;

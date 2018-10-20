c29f8bce801363868665f34e86308dea8de8fc0d
Edwin Cloud <ecloud412@gmail.com>
2018-10-20 13:57:03 -0700

    :mailbox_with_no_mail: Middlewares added: errorHandler and notFoundHandler. Better util imports.

--- utils/notfound.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.all('*', (req, res) => {
-  res.status(404).json({
-    message: `Not Found - ${req.originalUrl}`
-  });
-});
-
-module.exports = router;  

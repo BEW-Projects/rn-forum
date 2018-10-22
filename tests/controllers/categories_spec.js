ad5d3aa97666938f51277771a982a000daddf219
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-10-22 12:53:00 -0700

    categories test

---
+++ tests/controllers/categories_spec.js
@@ -0,0 +1,68 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../server.js');
+const should = chai.should();
+const mongoose = require('mongoose');
+const { category } = require('../../models');
+
+// setup chai to use http assertion
+chai.use(chaiHttp);
+
+// create our testCategory
+const testCategory = {
+  name: 'testname',
+  weight: 2,
+  author: mongoose.Types.ObjectId(),
+  color: 'blue'
+};
+
+// start our tests
+describe('Categories', () => {
+
+  // delete test categories after testing completed
+  after(async () => {
+    await category.deleteMany({ name: 'testname' })
+  })
+
+  // get all test
+  it('should return json for request at /categories GET', async () => {
+    const res = await chai.request(server).get(`/categories`);
+    res.should.have.status(200);
+    res.should.be.json;
+  });
+
+  // get one test
+  it('should return json for request at /categories?_id= GET', async () => {
+    const newCategory = await category.create(testCategory);
+    const res = await chai.request(server).get(`/categories?_id=${newCategory._id}`);
+    res.should.have.status(200);
+    res.should.be.json;
+  });
+
+  // create one test
+  it('should return json for request at /categories?_id= CREATE', async () => {
+    const res = await chai.request(server).post(`/categories`).send(testCategory);
+    res.should.have.status(200);
+    res.should.be.json;
+  });
+
+  // update one test
+  it('should return json for request at /categories?_id= UPDATE', async () => {
+    const newCategory = await category.create(testCategory);
+    const updates = {
+      weight: 3
+    };
+    const res = await chai.request(server).put(`/categories?_id=${newCategory._id}`).send(updates);
+    res.should.have.status(200);
+    res.should.be.json;
+  });
+
+  // delete one test
+  it('should return json for request at /categories?_id= DELETE', async () => {
+    const newCategory = await category.create(testCategory);
+    const res = await chai.request(server).delete(`/categories?_id=${newCategory._id}`);
+    res.should.have.status(200);
+    res.should.be.json;
+  });
+
+});

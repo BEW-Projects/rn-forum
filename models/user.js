9e4199ed9e26efc9da204a298bbc40f93832129a
Edwin Cloud <ecloud412@gmail.com>
2018-10-22 23:35:39 -0700

    :lock: User model added email validator and authenticate static method

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const UserSchema = mongoose.Schema({
   username: {
@@ -22,9 +24,28 @@ const UserSchema = mongoose.Schema({
   },
   role: {
     type: Number,
+    enum: [0, 1, 2, 3],
     required: true,
     default: 0   // 0 - User, 1 - Moderator, 2 - Developer, 3 - Administrator
   }
 });
+
+// custom validators
+UserSchema.path('email').validate(function(v) {
+  return validator.isEmail(v);
+})
+
+// authenticate a user
+UserSchema.statics.authenticate = async function(email, password) {
+  const user = await this.find({ email: email }).limit(1).lean();
+  if(user.length == 0) {
+    return new Error(`Email not found.`);
+  }
+  const match = await bcrypt.compare(password, user[0].password);
+  if(match) {
+    return user[0];
+  }
+  return new Error(`Invalid Password.`);
+}
 
 module.exports = mongoose.model('User', UserSchema);
